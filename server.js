7c9429c493436eb41a153f76d4b34e304b507bd2
Lakyn <lakynmphotography@hotmail.com>
2021-07-27 12:38:57 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
 const { animals } = require('./data/animals');
-
+const express = require('express');
 const app = express();
 
-app.get('/api/animasls', (req,res) => {
-    res.send('Hello');
-});
-
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    console.log(req.query)
+    res.json(results);
+  });
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
