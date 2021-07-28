964d6aae171e2de3be01e1eebec573f48bfc588b
Lakyn <lakynmphotography@hotmail.com>
2021-07-27 20:28:47 -0400

    changes

--- server.js
+++ server.js
@@ -101,6 +101,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
